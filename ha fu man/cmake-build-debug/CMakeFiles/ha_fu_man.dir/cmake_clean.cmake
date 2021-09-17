f31d44d083ac18ea17f68ec905604fa40dd6d651
sbzxp250 <89115833+sbzxp250@users.noreply.github.com>
2021-09-17 22:51:24 +0800

    Add files via upload

---
+++ ha fu man/cmake-build-debug/CMakeFiles/ha_fu_man.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ha_fu_man.dir/main.cpp.o"
+  "ha_fu_man"
+  "ha_fu_man.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ha_fu_man.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
